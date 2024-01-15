668e9b20d526074aab56c4d979b02b4d1d4a47da
luk2010 <jacques.tronconi@gmail.com>
2024-01-15 20:27:27 +0100

    Changes to be committed: 	modified:   .gitignore 	modified:   CMakeLists.txt 	new file:   Core/CMakeLists.txt 	new file:   Core/Pl/Application.h 	new file:   Core/Pl/OSX/Application.mm 	new file:   Core/config.h.in 	new file:   Examples/CMakeLists.txt 	new file:   Examples/HelloWorld/CMakeLists.txt     new file:   Examples/HelloWorld/main.cpp 	new file:   Modules/CMakeLists.txt 	new file:   Plugins/CMakeLists.txt

---
+++ Examples/HelloWorld/main.cpp
@@ -0,0 +1,11 @@
+//! 
+//! @file Pluton/Examples/HelloWorld/main.cpp
+//! @author Luk2010
+//! @date 2024/01/15
+//! @copyright Atlanti's Corp(r) 2024
+//! 
+
+int main(int argc, char** argv) 
+{
+    return 0;
+}
